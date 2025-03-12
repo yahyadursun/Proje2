bfd311c18eb30bb3109bbdc9549af582ab38b0c6
Tuğberk Erin <tugberkerin2002@hotmail.com>
2025-03-12 16:15:24 +0300

    mainPushTugberk
    server dosyamızın -->npm init --yes
    npm i express dotenv cors cookie-parser mongoose bcrypt jsonwebtoken
    package.json dosyamız için-->  npm i nodemon --save-dev
    jsconfig.json dosyasındaki hatayı çözemedim kb.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import {Button} from '@/components/ui/button'
-
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Auth from "./pages/auth";
+import Chat from "./pages/chat";
+import Profile from "./pages/profile";
 
 function App() {
   return (
     <div>
       <div>
-        <Button> click me</Button>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/auth" element={<Auth />}/>
+            <Route path="/chat" element={<Chat />}/>
+            <Route path="/profile" element={<Profile />}/>
+            <Route path="*" element={<Navigate to="/auth"/>}/>
+          </Routes>
+        </BrowserRouter>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/pages/auth/index.jsx
+++ client/src/pages/auth/index.jsx
@@ -1,12 +1,112 @@
+import Background from "@/assets/login2.png";
+import Victory from "../../assets/victory.svg";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "../../components/ui/tabs";
+import { useState } from "react";
+import { Input } from "../../components/ui/input";
+import { Button } from "../../components/ui/button";
 
-const Auth
- = () => {
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleLogin = async () => {};
+  const handleSignup = async () => {};
   return (
-    <div>
-      
+    <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+      <div className="h-[80vh] bg-white border-2 border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
+        <div className="flex flex-col gap-10 items-center justify-center">
+          <div className="flex items-center justify-center flex-col">
+            <div className="flex items-center justify-center">
+              <h1 className="text-5xl font-bold md:text-6xl ml-5">Hoşgeldiniz</h1>
+              <img
+                src={Victory}
+                alt="Victory emoji"
+                className="h-[100px]"
+              ></img>
+            </div>
+            <p className="font-medium text-center">
+              Burada olmak harika
+            </p>
+          </div>
+          <div className="flex items-center justify-center w-full">
+            <Tabs className={"w-3/4"}>
+              <TabsList className="bg-transparent rotate-none w-full ">
+                <TabsTrigger
+                  value="login"
+                  className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
+                >
+                  Login
+                </TabsTrigger>
+                <TabsTrigger
+                  value="signup"
+                  className="data-[state=active]:bg-transparent text-black text-opacity-90 border-b-2 rounded-none w-full data-[state=active]:text-black data-[state=active]:font-semibold data-[state=active]:border-b-purple-500 p-3 transition-all duration-300"
+                >
+                  SignUp
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent className=" flex flex-col gap-5 mt-5" value="login">
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-full p-6"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button className={"rounded-full p-6"} onClick={handleLogin}>
+                  Login
+                </Button>
+              </TabsContent>
+              <TabsContent
+                className=" flex flex-col gap-5 mt-5 "
+                value="signup"
+              >
+                <Input
+                  placeholder="Email"
+                  type="email"
+                  className="rounded-full p-6"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />{" "}
+                <Input
+                  placeholder="Confirm Password"
+                  type="password"
+                  className="rounded-full p-6"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <Button className={"rounded-full p-6"} onClick={handleSignup}>
+                  SignUp
+                </Button>
+              </TabsContent>
+            </Tabs>
+          </div>
+        </div>
+        <div className="hidden xl:flex justify-center items-center">
+          <img src={Background} alt="background" className="h-[auto]" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Auth
-
+export default Auth;
