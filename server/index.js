27f452af4a8df04c78777833dcd279552fe6e292
Tuğberk Erin <tugberkerin2002@hotmail.com>
2025-03-14 12:09:03 +0300

    .env - index.js yapılandırıldı

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+const databaseURL = process.env.DATABASE_URL;
+
+app.use(
+  cors({
+    origin: [process.env.ORIGIN],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+
+const server = app.listen(port, () => {
+  console.log(`Server is runnin at http://localhost:${port}`);
+});
+mongoose
+  .connect(databaseURL)
+  .then(() =>
+    console
+      .log("DB Connection Succesfull")
+      .catch((err) => console.log(err.message))
+  );
